3a8943c3f8a9fcdb1e7e438ff963de17caefd898
LauraTalicia <lauratalitaandrzejewska@gmail.com>
2023-02-28 20:35:33 +0100

    Init project

---
+++ components/intro.tsx
@@ -0,0 +1,20 @@
+import { CMS_NAME } from '../lib/constants'
+
+const Intro = () => {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Laura Talicia
+      </h1>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <span>Home </span>
+      <span>My Projects</span>
+      <span> About me</span>
+
+
+      </h4>
+    </section>
+  )
+}
+
+export default Intro
