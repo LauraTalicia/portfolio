7423d93b37e389b733c567895a76321b2cce85e7
LauraTalicia <lauratalitaandrzejewska@gmail.com>
2023-03-01 16:29:07 +0100

    Pritify, add menu links, change titles

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,20 +1,25 @@
-import { CMS_NAME } from '../lib/constants'
+import Link from "next/link";
+import { CMS_NAME } from "../lib/constants";
 
-const Intro = () => {
+const Intro = ({ title }: { title: string }) => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Laura Talicia
+        {title}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-      <span>Home </span>
-      <span>My Projects</span>
-      <span> About me</span>
-
-
+        <span>
+          <Link href="/"> Home</Link>
+        </span>
+        <span>
+          <Link href="/projects"> My Projects</Link>
+        </span>
+        <span>
+          <Link href="/about"> About me</Link>
+        </span>
       </h4>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,28 +1,28 @@
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
-import Layout from "../components/layout";
-import { getAllPosts } from "../lib/api";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
-import Post from "../interfaces/post";
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../interfaces/post'
 
 type Props = {
-  allPosts: Post[];
-};
+  allPosts: Post[]
+}
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
         <Head>
-          <title>{`Laura Talicia`}</title>
+          <title>About me</title>
         </Head>
         <Container>
-          <Intro title="Laura Talicia" />
+          <Intro title="About me"/>
           {heroPost && (
             <HeroPost
               title={heroPost.title}
@@ -37,20 +37,20 @@ export default function Index({ allPosts }: Props) {
         </Container>
       </Layout>
     </>
-  );
+  )
 }
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "author",
-    "coverImage",
-    "excerpt",
-  ]);
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
 
   return {
     props: { allPosts },
-  };
-};
+  }
+}
