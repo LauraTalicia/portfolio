964d77758181720d55eb5f08db55c579eca3c0e6
LauraTalicia <lauratalitaandrzejewska@gmail.com>
2023-03-04 19:16:17 +0100

    Add yellow squares to projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,20 +1,20 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../interfaces/post'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPosts } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import Post from "../interfaces/post";
 
 type Props = {
-  allPosts: Post[]
-}
-
+  allPosts: Post[];
+};
+const projects = ["rabyl", "skaj", "zuma", "czejs", "marszal", "roki"];
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
@@ -22,8 +22,17 @@ export default function Index({ allPosts }: Props) {
           <title>My Projects</title>
         </Head>
         <Container>
-          <Intro title="My Projects"/>
-          {heroPost && (
+          <Intro title="My Projects" />
+          <div className="flex flex-wrap justify-center">
+            {projects.map((project) => {
+              return (
+                <div className="bg-yellow-300 w-1/4 aspect-square flex flex-col items-center justify-center m-4">
+                  {project}
+                </div>
+              );
+            })}
+          </div>
+          {/* {heroPost && (
             <HeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
@@ -32,25 +41,25 @@ export default function Index({ allPosts }: Props) {
               slug={heroPost.slug}
               excerpt={heroPost.excerpt}
             />
-          )}
+          )} */}
           {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
+  ]);
 
   return {
     props: { allPosts },
-  }
-}
+  };
+};
